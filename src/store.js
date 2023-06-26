14bca2fcac4c7c1ad323b9a1f5000b7aec24c0a5
michelecanini <studio.michelecanini@gmail.com>
2023-06-26 19:55:14 +0200

    [UPDATE] Inserted api key in the store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,15 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
- searchText: "",
- loading: true,
+
+    searchText: "",
+    loading: true,
+
+    apiKey: '0c109132d12a812dd280ce879c2b7239',
+    moviesList: [],
+    tvSeriesList: [],
+    moviesApiUri: 'https://api.themoviedb.org/3/search/movie',
+    seriesApiUri: 'https://api.themoviedb.org/3/search/tv',
+    imageUri: 'https://image.tmdb.org/t/p/w342'
+
 });
