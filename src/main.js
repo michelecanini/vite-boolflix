81ec8dba1e29cbe4f2151cb61a49ebbed474c25f
michelecanini <studio.michelecanini@gmail.com>
2023-06-26 16:03:33 +0200

    [UPLOAD] Installed axios

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import * as bootstrap from 'bootstrap';
+import '@popperjs/core';
+import axios from 'axios';
+
+
 
 createApp(App).mount('#app')
