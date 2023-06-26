ab31af736033c75f4748d3d9ae18de43984b5f09
michelecanini <studio.michelecanini@gmail.com>
2023-06-26 16:39:26 +0200

    [UPLOAD] inserted and imported components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <script>
+
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue';
+import AppCard from './components/AppCard.vue';
+
 export default {
-  
+    components: {
+        AppHeader,
+        AppMain,
+        AppCard
+    }
 }
+
 </script>
 
 <template lang="">
+
   <div>
-    
+    <AppHeader />
+    <AppMain />
+    <AppCard />
   </div>
+
 </template>
 
 <style lang="scss">
