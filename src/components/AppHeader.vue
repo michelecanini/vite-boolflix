62d5c6609666328993d335c2a4f44a6160611498
michelecanini <studio.michelecanini@gmail.com>
2023-06-27 12:53:30 +0200

    [UPDATE] Inserted test call api axios

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,10 +9,10 @@ export default {
     <header>
         <div class="container d-flex justify-content-between">
             <div class="row d-flex "></div>
-            <div class="col-5  mt-2 ">
-                <span>Logo</span>
+            <div class="col-3 mt-2 ">
+                <span>BOOLFLIX</span>
             </div>
-            <div class="col-5  mt-2 ">
+            <div class="col-9 mt-2 ">
                 <div class="input-group">
                     <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
                     <button type="button" class="btn btn-outline-primary">search</button>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,20 @@
 <script>
+
+import axios from 'axios';
+
 export default {
- 
+    data(){
+        return{
+            moviesList: []
+        }
+    },
+    mounted(){
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=0c109132d12a812dd280ce879c2b7239&query=matrix').then ((response) => {
+            console.log(response.data);
+        })
+    }
 }
+
 </script>
 
 <template lang="">
