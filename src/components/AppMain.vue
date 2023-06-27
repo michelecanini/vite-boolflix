4c00f7aa3a4e697d4ae6d056cece829326c0b7fb
michelecanini <studio.michelecanini@gmail.com>
2023-06-27 14:06:28 +0200

    [UPDATE] Inserted four value movie

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,7 +10,7 @@ export default {
     },
     mounted(){
         axios.get('https://api.themoviedb.org/3/search/movie?api_key=0c109132d12a812dd280ce879c2b7239&query=matrix').then ((response) => {
-            console.log(response.data);
+            this.moviesList = response.data.results;
         })
     }
 }
@@ -18,9 +18,18 @@ export default {
 </script>
 
 <template lang="">
- <div>
-  
- </div>
+
+    <div class="container">
+        <div class="row">
+            <div v-for="(movie, index) in moviesList" v-bind:key="movie.id" class="col-6 col-md-4 mb-2">
+                {{ movie.title }} <br>
+                {{ movie.original_title }} <br>
+                {{ movie.original_language }} <br>
+                {{ movie.vote_count }} <br>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <style lang="">
