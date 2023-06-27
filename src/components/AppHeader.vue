472895692a17c9521c6fc601588f0be8a976bca6
michelecanini <studio.michelecanini@gmail.com>
2023-06-27 11:16:07 +0200

    [UPDATE] inserted input search and button

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,22 @@ export default {
 </script>
 
 <template lang="">
- <div>
-  
- </div>
+
+    <header>
+        <div class="container d-flex justify-content-between">
+            <div class="row d-flex "></div>
+            <div class="col-5  mt-2 ">
+                <span>Logo</span>
+            </div>
+            <div class="col-5  mt-2 ">
+                <div class="input-group">
+                    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                    <button type="button" class="btn btn-outline-primary">search</button>
+                </div>
+            </div>
+        </div>
+    </header>
+
 </template>
 
 <style lang="">
