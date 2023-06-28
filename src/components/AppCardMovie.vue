f1f307f44cd3471bd996647d84436392d324ce99
michelecanini <studio.michelecanini@gmail.com>
2023-06-28 19:32:51 +0200

    [FIX] Inserted vote_average

--- src/components/AppCardMovie.vue
+++ src/components/AppCardMovie.vue
@@ -12,15 +12,15 @@ export default {
     <div>
         <img :src="`https://image.tmdb.org/t/p/w342/${myMovie.poster_path}`">
         <ul>
-            <li>Titolo: <strong>{{ myMovie.title }}</strong></li>
+            <li>Titolo Film: <strong>{{ myMovie.title }}</strong></li>
             <li>Titolo Originale: <strong>{{ myMovie.original_title }}</strong></li>
             <li>Lingua: <strong>{{ myMovie.original_language }}</strong></li>
-            <li>Voto: <strong>{{ myMovie.vote_count }}</strong></li>
+            <li>Voto: <strong>{{ myMovie.vote_average }}</strong></li>
         </ul>
     </div>
   
 </template>
 
 <style lang="" >
- 
+    
 </style>
--- src/components/AppCardTv.vue
+++ src/components/AppCardTv.vue
@@ -11,10 +11,10 @@ export default {
     <div>
         <img :src="`https://image.tmdb.org/t/p/w342/${mySerie.poster_path}`">
         <ul>
-            <li>Titolo: <strong>{{ mySerie.name }}</strong></li>
+            <li>Titolo Serie: <strong>{{ mySerie.name }}</strong></li>
             <li>Titolo Originale: <strong>{{ mySerie.original_name }}</strong></li>
             <li>Lingua: <strong>{{ mySerie.original_language }}</strong></li>
-            <li>Voto: <strong>{{ mySerie.vote_count }}</strong></li>
+            <li>Voto: <strong>{{ mySerie.vote_average }}</strong></li>
         </ul>
     </div>
 
