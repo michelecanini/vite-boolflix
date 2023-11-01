9f0d0f8e17cdcfb4553e8b97f20386e5e6d77a68
michelecanini <studio.michelecanini@gmail.com>
2023-11-01 08:53:11 +0100

    [TWEAK] Modified styling of the header component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,13 +14,13 @@ export default {
 
 <template lang="">
 
-    <header>
+<header>
         <div class="container d-flex justify-content-between">
-            <div class="row d-flex "></div>
-            <div class="col-3 mt-3 mb-3">
+            <div class="row d-flex"></div>
+            <div class="col-6 mt-3 mb-3">
                 <h1>BOOLFLIX</h1>
             </div>
-            <div class="col-9 mt-3 mb-3">
+            <div class="col-6 mt-4 mb-2">
                 <div class="input-group">
                     <input type="search" class="form-control rounded" placeholder="Cerca Film e Serie TV" aria-label="Search" aria-describedby="search-addon" v-model="store.searchText" @keyup.enter="$emit('search')"/>
                     <button type="button" class="btn btn-outline-primary" @click="$emit('search')">search</button>
@@ -31,6 +31,18 @@ export default {
 
 </template>
 
-<style lang="">
- 
+<style lang="scss">
+
+header{
+    position: fixed;
+    top: 0;
+    width: 100%;
+    background-color: #0e0e0e;
+    z-index: 1;
+
+    h1{
+        color: red;
+    }
+}
+
 </style>
