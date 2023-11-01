437551d003dc3fc4fe323f859dfe34d86b6e9744
michelecanini <studio.michelecanini@gmail.com>
2023-11-01 10:41:25 +0100

    [FEAT] Added style and error message to the AppCardTv component (TV series)

--- src/components/AppCardTv.vue
+++ src/components/AppCardTv.vue
@@ -1,25 +1,112 @@
 <script>
 export default {
- props: {
-       mySerie: Object
-   },
+    props: {
+        mySerie: Object
+    },
 }
 </script>
 
-<template lang="">
-
-    <div>
-        <img :src="`https://image.tmdb.org/t/p/w342/${mySerie.poster_path}`">
-        <ul>
-            <li>Titolo Serie: <strong>{{ mySerie.name }}</strong></li>
-            <li>Titolo Originale: <strong>{{ mySerie.original_name }}</strong></li>
-            <li>Lingua: <strong>{{ mySerie.original_language }}</strong></li>
-            <li>Voto: <strong>{{parseInt(Math.round(mySerie.vote_average / 2))}}</strong></li>
-        </ul>
+<template>
+    <div class="flip-card">
+        <div class="flip-card-inner" :class="{ 'prevent-rotate': !mySerie.poster_path }">
+            <div class="flip-card-front">
+                <img v-if="mySerie.poster_path" :src="`https://image.tmdb.org/t/p/w342/${mySerie.poster_path}`" class="serie-poster">
+                <div v-else class="fallback-cover text-white">Cover non disponibile</div>
+            </div>
+            <div class="flip-card-back mt-5" :class="{ 'fallback-cover-back': !mySerie.poster_path }">
+                <ul>
+                    <li>Titolo Serie:<br> <strong>{{ mySerie.name }}</strong></li>
+                    <li class="mt-2">Titolo Originale:<br> <strong>{{ mySerie.original_name }}</strong></li>
+                    <li class="mt-2">Lingua: <strong>{{ mySerie.original_language }}</strong></li>
+                    <li class="mt-2">Voto: <strong>{{parseInt(Math.round(mySerie.vote_average / 2))}}</strong></li>
+                </ul>
+            </div>
+        </div>
     </div>
-
 </template>
 
-<style lang="">
- 
+<style scoped>
+/* Il tuo stile esistente */
+
+.flip-card {
+    background-color: transparent;
+    perspective: 1000px;
+    margin-bottom: 650px;
+    filter: drop-shadow(0 0 20px rgba(0,0,255,0.5));
+}
+
+.flip-card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+}
+
+.flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+}
+
+.flip-card-front, .flip-card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+}
+
+.flip-card-back {
+    background-color: #000; 
+    color: white;
+    transform: rotateY(180deg);
+    font-size: 17px;
+}
+
+.flip-card:hover .flip-card-front img {
+    filter: brightness(0%);
+}
+
+.flip-card-back ul {
+    list-style-type: none; 
+    padding: 0; 
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center; 
+    height: 100%; 
+    padding-top: 20%; 
+    width: 300px; 
+    overflow-wrap: break-word;
+    margin: auto;
+}
+
+.fallback-cover {
+    color: red;
+    font-size: 20px;
+    padding-bottom: 50px;
+    padding-top: 50px;
+    background-color: rgb(255, 66, 66);
+}
+
+.flip-card:hover .fallback-cover {
+    opacity: 0;
+}
+
+.flip-card:hover .flip-card-back img{
+    filter: brightness(0%);
+    padding: 50px;
+    background-color: red;
+}
+
+.fallback-cover-back {
+    color: white;
+    font-size: 18px;
+    padding-bottom: 350px;
+    background-color: rgb(255, 66, 66);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
 </style>
