9e6557eed5ca64a68f02a6365ec68bc7ffb4b98b
michelecanini <studio.michelecanini@gmail.com>
2023-11-01 08:56:40 +0100

    [FEAT] Added carousel slider for films and TV series

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,32 +13,127 @@ export default {
     data() {
         return {
             store,
-        }
-    },
+            rotate: {}
+    }
+  },
+  
 }
 
 </script>
 
 <template lang="">
 
-    <div class="container"> 
-        <div class="row">
-            <div v-for="(movie, index) in store.moviesList" v-bind:key="movie.id" class="col-6 col-md-4 mb-2">
-            
-                 <AppCardMovie v-bind:myMovie="movie"/>
+ <div class="container">
+    <div class="row card_margin">
+      <div v-for="(movie, index) in store.moviesList" v-bind:key="movie.id" class="col-6 col-md-4 mt-5 mb-2 text-center">
+        <AppCardMovie v-bind:myMovie="movie" @mouseover.native="rotate[index] = true" @mouseout.native="rotate[index] = false" :class="{ 'rotate': rotate[index] }"/>
+      </div>
+      <div v-for="(serie, index) in store.tvSeriesList" v-bind:key="serie.id" class="col-6 col-md-4 mt-5 mb-2 text-center">
+        <AppCardTv v-bind:mySerie="serie" @mouseover.native="rotate[index] = true" @mouseout.native="rotate[index] = false" :class="{ 'rotate': rotate[index] }"/>
+      </div>
+    </div>
+  </div>
 
-            </div>
-            <div v-for="(serie, index) in store.tvSeriesList" v-bind:key="serie.id" class="col-6 col-md-4 mb-2">
-            
-                <AppCardTv v-bind:mySerie="serie"/>
-
-            </div>
-        </div>
+    <!--carosello slide-->
+    <div class="container-fluid text-center carousel_slide"> 
+        <h2 class="font-weight-light text-white">I Titoli del Momento</h2> 
+            <div class="row mx-auto my-auto justify-content-center"> 
+                <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel"> 
+                    <div class="carousel-inner" role="listbox"> 
+                        <div class="carousel-item active"> 
+                            <div class="col-md-2"> 
+                                <div class="card">   
+                                    <div class="card-img"> 
+                                        <img src="https://cdn.wallpapersafari.com/59/44/o2mLkf.jpg" class="img-fluid">
+                                    </div> 
+                                    <div class="card-img-overlay"> 
+                                    </div> 
+                                </div> 
+                            </div> 
+                        </div> 
+                        <div class="carousel-item test2"> 
+                            <div class="col-md-2"> 
+                                <div class="card"> 
+                                    <div class="card-img"> 
+                                        <img src="https://cdn.prod.mktg.evvnt.com/uploads/event_image/1939168/event_image/hero_Leo.webp" class="img-fluid">
+                                    </div> 
+                                    <div class="card-img-overlay"></div> 
+                                </div> 
+                            </div> 
+                        </div> 
+                        <div class="carousel-item"> 
+                            <div class="col-md-2"> 
+                                <div class="card"> 
+                                    <div class="card-img"> <img src="https://www.greaterfool.tv/test/wp-content/uploads/2022/10/film-hd-in-streaming.jpg" class="img-fluid"> </div> <div class="card-img-overlay">Slide 3</div> 
+                                </div> 
+                            </div> 
+                        </div> 
+                        <div class="carousel-item"> 
+                            <div class="col-md-2"> 
+                                <div class="card"> 
+                                    <div class="card-img"> <img src="https://i.ytimg.com/vi/1qIvQxLcyys/maxresdefault.jpg" class="img-fluid"> </div> 
+                                    <div class="card-img-overlay">Slide 4</div> 
+                                </div> 
+                            </div> 
+                        </div> 
+                        <div class="carousel-item"> 
+                            <div class="col-md-2"> 
+                                <div class="card"> 
+                                    <div class="card-img"> <img src="https://cdn.wallpapersafari.com/86/83/c7nmps.jpg" class="img-fluid"> </div> <div class="card-img-overlay">Slide 5</div> 
+                                </div> 
+                            </div> 
+                        </div> 
+                        <div class="carousel-item"> 
+                            <div class="col-md-2"> 
+                                <div class="card"> 
+                                    <div class="card-img"> <img src="https://wallpapercave.com/wp/gssEep9.jpg" class="img-fluid"> </div> <div class="card-img-overlay">Slide 6</div> 
+                                </div> 
+                            </div> 
+                        </div>
+                        <div class="carousel-item"> 
+                            <div class="col-md-2"> 
+                                <div class="card"> 
+                                    <div class="card-img"> <img src="https://img.wallscloud.net/uploads/thumb/2845750753/one-piece-rR9L-1024x576-MM-80.webp" class="img-fluid"> </div> <div class="card-img-overlay">Slide 7</div> 
+                                </div> 
+                            </div> 
+                        </div> 
+                        <div class="carousel-item"> 
+                            <div class="col-md-2"> 
+                                <div class="card"> 
+                                    <div class="card-img"> <img src="https://www.pixelstalk.net/wp-content/uploads/2016/07/1080p-Movie-Wallpapers-For-Desktop.jpg" class="img-fluid"> </div> <div class="card-img-overlay">Slide 8</div> 
+                                </div> 
+                            </div> 
+                        </div> 
+                    </div> 
+                    <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> </a> 
+                    <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> </a> 
+                </div> 
+            </div> 
     </div>
 
 </template>
 
 <style lang="scss">
 
- 
+@media (max-width: 767px){.carousel-inner .carousel-item >div{display: none}.carousel-inner .carousel-item >div:first-child{display: block}}.carousel-inner .carousel-item.active, .carousel-inner .carousel-item-next, .carousel-inner .carousel-item-prev{display: flex}@media (min-width: 768px){.carousel-inner .carousel-item-end.active, .carousel-inner .carousel-item-next{transform: translateX(25%)}.carousel-inner .carousel-item-start.active, .carousel-inner .carousel-item-prev{transform: translateX(-25%)}}.carousel-inner .carousel-item-end, .carousel-inner .carousel-item-start{transform: translateX(0)}
+
+.carousel_slide{
+    margin-bottom: 1rem !important;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+
+.card {
+  transition: transform .2s; /* Animation */
+}
+
+.card:hover {
+  transform: scale(1.2); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+}
+
+.card_margin{
+    margin-top: 6rem !important;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -5,3 +5,20 @@ import '@popperjs/core';
 import axios from 'axios';
 
 createApp(App).mount('#app')
+
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 6
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
